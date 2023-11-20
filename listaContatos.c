e9b896bc7d042179d5fb0041c5062b183b07033d
Eytor Lima <eytorlima@outlook.com>
2023-11-20 00:40:17 -0300

    primeiro commit

---
+++ listaContatos.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <locale.h>
+#include "listaContatos.h"
---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <locale.h> //vc usa o locale-ptbr? eu gosto, mas a questao da virgula no float me incomoda.
+#include "listaContatos.h"  
+
+int main(){
+    setlocale(LC_ALL, "Portuguese");
+
+    printf("\n\nteste, primeiro commit");
+
+    printf("\n\n");
+    system("pause");
+    return 0;
+}
